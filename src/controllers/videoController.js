bfc537f7d4cdbd87c840608658079fd7645748bb
charchar111 <3hjab@naver.com>
2023-04-23 12:56:21 +0900

    #6.13 -2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,22 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  console.log("search start");
-  Video.find({}, (error, videos) => {
-    console.log("error: ", error);
-    console.log("videos", videos);
-    console.log("search end");
-    return res.render("home", { pageTitle: "home", videos: [] });
-  });
+Video.find({}, (error, videos) => {
+  // console.log("search start");
+  // console.log("error: ", error);
+  // console.log("videos", videos);
+  // console.log("search end");
+});
+
+// export const home = (req, res) => {
+//   return res.render("home", { pageTitle: "home", videos: [] });
+// };
+//위 콜백 방식 /
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
 
