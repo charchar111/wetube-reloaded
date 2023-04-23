94183f95c8b939e4f2f00d64f448cee7e7b4d5fb
charchar111 <3hjab@naver.com>
2023-04-23 15:01:32 +0900

    #6.13 -3

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,21 +1,42 @@
 import Video from "../models/Video";
 
-Video.find({}, (error, videos) => {
-  // console.log("search start");
-  // console.log("error: ", error);
-  // console.log("videos", videos);
-  // console.log("search end");
-});
+// Video.find({}, (error, videos) => {
+//   // console.log("search start");
+//   // console.log("error: ", error);
+//   // console.log("videos", videos);
+//   // console.log("search end");
+// });
 
 // export const home = (req, res) => {
 //   return res.render("home", { pageTitle: "home", videos: [] });
 // };
-//위 콜백 방식 /
+//위 콜백 방식 고친거/
+
+// export const home = (req, res) => {
+//   console.log("search start");
+//   Video.find({}, (error, videos) => {
+//     // console.log("error: ", error);
+//     // console.log("videos", videos);
+//     console.log("search end");
+//     console.log(videos);
+//   });
+//   return res.render("home", { pageTitle: "home", videos: [] });
+// };
+//위, 비동기화 콜백 안고친거/
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
-  return res.render("home", { pageTitle: "Home", videos });
+  try {
+    console.log("i start");
+    const videos = await Video.find({});
+    console.log("i finished");
+    console.log(videos);
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log("server error:", error);
+    return res.render("serverError");
+  }
 };
+//동기적, try= 정상 실행, catch = 에러 발생 시/;
 
 export const watch = (req, res) => {
   const { id } = req.params;
