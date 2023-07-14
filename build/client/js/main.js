69f798c355b28107d6da5615e94c420c086d87c5
charchar111 <3hjab@naver.com>
2023-07-14 18:36:29 +0900

    #16(complete)

--- build/client/js/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-require("../scss/styles.scss");
-var _regeneratorRuntime = _interopRequireDefault(require("regenerator-runtime"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-console.log("main");
